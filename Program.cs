5126b125b7e9dd9798d2143069cc70fbe073d9a7
IgorVicente <igor_machado05@hotmail.com>
2022-04-04 13:49:10 -0300

    criado funcao soma e exibindo valores

--- Program.cs
+++ Program.cs
@@ -4,6 +4,18 @@
         {
             Console.WriteLine("Hello, branch master!");
             Console.WriteLine("Projeto inicial");
+
+            System.Console.WriteLine("digite o primeiro número a ser somado: ");
+            int number1 = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("digite o segundo número a ser somado: ");
+            int number2 = int.Parse(Console.ReadLine());
+
+            int soma = Soma(number1, number2);
+            System.Console.WriteLine("resultado: " + soma);
+        }
+
+        static int Soma(int number1, int number2){
+            return number1 + number2;
         }
     }
 
