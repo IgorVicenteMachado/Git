5c9483766e82e0d4c28ec6b438d4e6551551f0ab
IgorVicente <igor_machado05@hotmail.com>
2022-04-04 14:46:11 -0300

    funcao soma e subtracao adicionado

--- Program.cs
+++ Program.cs
@@ -1,21 +1,44 @@
 ﻿namespace brincandogit;
     class program {
         static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, branch master!");
+        Console.WriteLine("Projeto inicial");
+
+        System.Console.WriteLine("digite o primeiro número: ");
+        int number1 = int.Parse(Console.ReadLine());
+        System.Console.WriteLine("digite o segundo número: ");
+        int number2 = int.Parse(Console.ReadLine());
+
+        Menu(number1, number2);
+
+        Console.ReadKey();
+    }
+
+    private static void Menu(int number1, int number2)
+    {
+        Console.WriteLine("Somar ou Dividir?");
+        Console.WriteLine("Pressione [s] para somar");
+        Console.WriteLine("Pressione [d] para subtrair");
+        char opcao = Convert.ToChar(Console.ReadLine().ToLower());
+
+        switch (opcao)
         {
-            Console.WriteLine("Hello, branch master!");
-            Console.WriteLine("Projeto inicial");
-
-            System.Console.WriteLine("digite o primeiro número a ser somado: ");
-            int number1 = int.Parse(Console.ReadLine());
-            System.Console.WriteLine("digite o segundo número a ser somado: ");
-            int number2 = int.Parse(Console.ReadLine());
-
-            int soma = Soma(number1, number2);
-            System.Console.WriteLine("resultado: " + soma);
+            case 's': Somar(number1, number2); break;
+            case 'd': Subtrair(number1, number2); break;
+            default: Menu(number1, number2); break;
         }
+    }
 
-        static int Soma(int number1, int number2){
-            return number1 + number2;
+    static int Somar(int number1, int number2){
+            int res = number1 + number2;
+            System.Console.WriteLine("resultado: " + res);
+            return res;
+        }
+          static int Subtrair(int number1, int number2){
+            int res = number1 - number2;
+            System.Console.WriteLine("resultado: " + res);
+            return res;
         }
     }
 
